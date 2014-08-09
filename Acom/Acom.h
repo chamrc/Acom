3177c1dec207328282d0d09a0955d190a1911270
Yasuharu Yanamura <yanamura3@gmail.com>
2014-08-09 10:15:41 +0900

    Initial Commit

---
+++ Acom/Acom.h
@@ -0,0 +1,19 @@
+//
+//  Acom.h
+//  Acom
+//
+//  Created by yanamura on 2014/08/09.
+//  Copyright (c) 2014年 Yasuharu Yanamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Acom.
+FOUNDATION_EXPORT double AcomVersionNumber;
+
+//! Project version string for Acom.
+FOUNDATION_EXPORT const unsigned char AcomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Acom/PublicHeader.h>
+
+
